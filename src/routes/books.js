c673696e39b280a0529cd6156dd799d2e855eccf
Abel AR <abel_john_14@hotmail.com>
2021-07-11 16:57:08 -0500

    Add post in the rest api

--- src/routes/books.js
+++ src/routes/books.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const mysqlConnection = require("../db/db");
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
     mysqlConnection.query('SELECT * FROM books', (error, rows, fields) => {
         if(!error) {
             res.json(rows);
@@ -11,6 +11,38 @@ router.get("/", (req, res) => {
             console.log(error);
         }
     })
-})
+});
+
+router.get('/:book_id', (req, res) => {
+    const { book_id } = req.params;
+    mysqlConnection.query('SELECT * FROM books WHERE book_id = ?', [book_id], (error, rows, fields) => {
+        if(!error) {
+            res.json(rows);
+        } else {
+            console.log(error);
+        }
+    });
+});
+
+        // SET @book_id = ?;
+        // SET @book_name = ?;
+        // SET @url_image = ?;
+        // SET @book_price = ?;
+        // SET @book_discount;
+        // SET @category_id;
+
+router.post('/', (req, res) => {
+    const { book_id, book_name, url_image, book_price, book_discount, category_id } = req.body;
+    const query =  `
+        CALL addBookOrEdit(?, ?, ?, ?, ?, ?);
+    `;
+    mysqlConnection.query(query, [ book_id, book_name, url_image, book_price, book_discount, category_id ], (error, rows,  fields) => {
+        if(!error) {
+            res.json({Status: 'Book saved'});
+        } else {
+            console.log(error);
+        }
+    });
+});
 
 module.exports = router;
